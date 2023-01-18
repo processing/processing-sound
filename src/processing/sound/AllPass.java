904582b449f9cadfa4a1501d9ce7ba3dffec2b97
Jonny Watson <PixMusix@gmail.com>
2023-01-19 09:12:46 +1100

    Added comments, including @webref and @webBrief comments for autodocumentation. Added simple logic to cap argument of the gain() method to within a reasonable range.

--- src/processing/sound/AllPass.java
+++ src/processing/sound/AllPass.java
@@ -6,7 +6,12 @@ import processing.core.PApplet;
 
 /**
  * This is a all pass filter.
+ * For signals processed, all frequencies hold the same amplitude 
+ * but have their phase relationship modified using a delayline of one sample.
+ * 	>> y(k) = -z * x(k) + x(k - 1) + z * y(k - 1)
+ * Where: y is the output, x is the input, z is the gain coefficient, and k is the signal.
  * @webref effects
+ * @webBrief This is a all pass filter. It outputs all frequencies at the same amplitude but changes their phase relationship.
  * @param parent PApplet: typically use "this"
  **/
 public class AllPass extends Effect<FilterAllPass> {
@@ -20,7 +25,18 @@ public class AllPass extends Effect<FilterAllPass> {
 		return new FilterAllPass();
 	}
 
+	/**
+	 * Set the gain for the filter.
+	 * @webref allpass
+	 * @webBrief Set the gain for the filter. Takes float from 0.0 - 1.0 where larger values increase phase displacement.
+	 * @param freq Bandwidth in Hz
+	 **/
 	public void gain(float g) {
+		// Keep the user from throwing bad output signals.
+		if (g < 0.0) { g = 0.0f; }
+		if (g > 1.0) { g = 1.0f; }
+
+		//Set the gain of the effect.
 		this.left.gain.set(g);
 		this.right.gain.set(g);
 	}
