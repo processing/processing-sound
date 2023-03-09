854c171570052fbe43ed766c618e3e9348290b49
kevinstadler <kevinstadler@users.noreply.github.com>
2023-03-09 14:30:51 +0100

    Separate Github action workflows and update Javadoc preamble

--- src/processing/sound/package-info.java
+++ src/processing/sound/package-info.java
@@ -1,4 +1,4 @@
 /**
- * <b>This Javadoc is only provided as reference for advanced users.</b> <b>For the basic documentation of the Processing Sound library with examples, please see <a href="https://processing.org/reference/libraries/sound/" target="_top">https://processing.org/reference/libraries/sound/</a>.</b>
+ * <b>This Javadoc is only provided as reference for advanced users.</b> It might describe functionality that is not yet available in official Sound library releases, but only in development test builds that can be downloaded from Github. New functionality might still be subject to change. <b>For the basic documentation of the Processing Sound library with examples, please see <a href="https://processing.org/reference/libraries/sound/" target="_top">https://processing.org/reference/libraries/sound/</a>.</b>
  */
 package processing.sound;
