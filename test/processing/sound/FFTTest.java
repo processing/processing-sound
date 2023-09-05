27e5fe95c0dec8d029ef4623c5bd512a0e538112
kevinstadler <kevinstadler@users.noreply.github.com>
2023-09-05 22:03:50 +0200

    Add first dummy tests for testing

---
+++ test/processing/sound/FFTTest.java
@@ -0,0 +1,13 @@
+package processing.sound;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class FFTTest {
+
+	@Test
+	public void testSomething() {
+		assertEquals(2, 1+1);
+	}
+}
+
---
+++ test/processing/sound/SoundTest.java
@@ -0,0 +1,20 @@
+package processing.sound;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class SoundTest {
+
+	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+	@Test
+	public void testList() {
+		System.setOut(new PrintStream(outputStreamCaptor));
+		Sound.list();
+		assertEquals("asd", outputStreamCaptor.toString());
+	}
+}
+
